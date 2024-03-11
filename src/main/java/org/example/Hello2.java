37f06e83eb4c12c83da0e3cc1ca19c7571647b71
980613juyoung <skcd165@naver.com>
2024-03-11 17:43:10 +0900

    소스 수정

---
+++ src/main/java/org/example/Hello2.java
@@ -0,0 +1,7 @@
+package org.example;
+public class Hello2 {public static void main(String[] args) {
+    System.out.println("Hello2!! My First Java Coding!!!!");
+    System.out.println("Edit Java Code!!");
+}
+}
+
