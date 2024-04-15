d0eaac042f442a36798f506a27cdd89d2ffc2ab8
980613juyoung <skcd165@naver.com>
2024-04-15 14:41:31 +0900

    Code06_06

---
+++ src/main/java/org/example/Quiz4.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Quiz4 {
+    public static void main(String[] args) {
+        for (int k = 2; k <=10; k++) {
+
+            for (int i = 1; i <= 9; i++) {
+                System.out.println(k + " x " + i + " = " + k * i);
+            }
+        }
+    }
+}
