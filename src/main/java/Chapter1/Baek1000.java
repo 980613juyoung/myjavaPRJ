dbdc7564974938f87b1048fca6863880dbe83fb0
980613juyoung <skcd165@naver.com>
2024-05-06 17:26:50 +0900

    240506

---
+++ src/main/java/Chapter1/Baek1000.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Baek1000{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt(); int b = s.nextInt();
+        int jooyoung = a+b;
+        System.out.println(jooyoung);
+    }
+}
