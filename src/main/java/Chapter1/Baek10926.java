3498771ddafb949a5597f347a977084f7264c4b8
980613juyoung <skcd165@naver.com>
2024-05-31 20:38:19 +0900

    240531

---
+++ src/main/java/Chapter1/Baek10926.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Baek10926 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String p = s.next();
+        String a = "??!";
+        System.out.print(p + a);
+    }
+}
