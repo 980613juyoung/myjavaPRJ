3b152755f7bdf99e269c2da23e72f0634463a720
980613juyoung <skcd165@naver.com>
2024-06-05 20:57:25 +0900

    240605

---
+++ src/main/java/Chapter1/Baek10998.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Baek10998 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        long a = s.nextInt();
+        long b = s.nextInt();
+        System.out.println(a * b);
+    }
+}
